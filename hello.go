ce979a8914c503991249e60c156d5c07c0adeaa0
Sandeep Singh <sandeep.singh03@sap.com>
2021-06-29 23:30:59 +0530

    added method logic

--- hello.go
+++ hello.go
@@ -2,8 +2,8 @@ package main
 import (
 	"fmt"
 	// "encoding/json"
-	"net/http"
-	"io/ioutil"
+	// "net/http"
+	// "io/ioutil"
 	// "github.com/sdputurn/learn-go"
 	// "github.com/sdputurn/learn-go/patterns"
 	// "hello/patterns"
@@ -30,13 +30,32 @@ func main () {
 	// err := json.Unmarshal([]byte(j), &stu)
 	// fmt.Println(err)
 	// fmt.Printf("%T - %v \n",stu,stu)
-	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts/1")
-	if err != nil {
-		fmt.Println(err)
-	}
-	body, err := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(body))
+	// resp, err := http.Get("https://jsonplaceholder.typicode.com/posts/1")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// body, err := ioutil.ReadAll(resp.Body)
+	// fmt.Println(string(body))
+	a1:=account{"sandeep",1000}
+	a1.creditMoney(100)
+	fmt.Println(a1)
+	a1.debitMoney(200)
+	fmt.Println("after debit - ",a1)
+
 
 
 }
 
+type account struct {
+	name string
+	balance int
+}
+
+func (acc *account) creditMoney(amt int) {
+	fmt.Printf("+++++++++++++money added %s added \n", acc.name)
+	acc.balance = acc.balance + amt
+	// return name
+}
+func (acc *account) debitMoney(amt int) {
+	acc.balance = acc.balance - amt
+}
